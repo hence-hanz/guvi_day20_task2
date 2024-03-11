60c2985f1492411d2389089e8bc8ed3c1da928ff
Haneesh p <116893066+hence-hanz@users.noreply.github.com>
2024-03-12 00:16:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const dog_btn = document.getElementById('dog_btn');
+const dog_result = document.getElementById('dog_result');
+dog_btn.addEventListener('click', getRandomDog);
+//fetching random dog image from random.dog api
+function getRandomDog() {
+	fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+			}
+		});
+}
